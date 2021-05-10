3f4c593a691eb4713c983cc1a363a2f84df00e73
mashabiarya <aryakasbi@gmail.com>
2021-05-11 01:45:55 +0700

    11_Redux_Thunk

---
+++ src/11_Redux_Thunk/src/firebase.config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB69vMb95TrxUPa_OWuoAA8AEsc7jmzFjY",
+    authDomain: "belajar-react-be1af.firebaseapp.com",
+    projectId: "belajar-react-be1af",
+    storageBucket: "belajar-react-be1af.appspot.com",
+    messagingSenderId: "210278288039",
+    appId: "1:210278288039:web:11bc300c1e079d3d4b6982",
+    measurementId: "G-X9G4WC2CRV"
+  };
+
+  export const myFirebase = firebase.initializeApp(firebaseConfig);
